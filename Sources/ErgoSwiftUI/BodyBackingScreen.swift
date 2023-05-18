5a7367883d9f9df2bb034837d16915307d444dd6
Jordan Kay <jordan@fleuronic.com>
2023-05-18 19:02:25 -0400

    Update dependencies

--- Sources/ErgoSwiftUI/BodyBackingScreen.swift
+++ Sources/ErgoSwiftUI/BodyBackingScreen.swift
@@ -2,7 +2,6 @@
 
 import SwiftUI
 import WorkflowUI
-import Inject
 
 public protocol BodyBackingScreen: Screen where View.Screen == Self {
 	associatedtype View: BodyProvider
@@ -13,9 +12,9 @@ public extension BodyBackingScreen {
 	// MARK: Screen
 	func viewControllerDescription(environment: ViewEnvironment) -> ViewControllerDescription {
 		.init(
-			type: Inject.ViewControllerHost<BodyViewController<View>>.self,
-			build: { .init(.init(screen: self, environment: environment)) },
-			update: { $0.instance.update(screen: self, environment: environment) }
+			type: BodyViewController<View>.self,
+			build: { .init(screen: self, environment: environment) },
+			update: { $0.update(screen: self, environment: environment) }
 		)
 	}
 }
